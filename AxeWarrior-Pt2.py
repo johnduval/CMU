185f9ccd9b5de86f0b329e74931d25b830cd106a
John DuVal <john.duval@mtbethelchristian.org>
2020-03-17 19:51:26 -0400

    Create AxeWarrior-Pt2.py

---
+++ AxeWarrior-Pt2.py
@@ -0,0 +1,28 @@
+#############################
+###                       ###
+### AXE WARRIOR - PART 1  ###
+###                       ###
+#############################
+
+# Create global variables.
+# Modify initializeGame (taken from tic-tac-toe)
+# Create player's starting Hit Points (HP) in initializeGame()
+# Create player movement (bounded motion)
+# Obstacles
+    # Unable to cross Mountains
+    
+    
+#############################
+###                       ###
+### AXE WARRIOR - PART 2  ###
+###                       ###
+#############################
+
+# Gain / Loose HP from stepping on to fire & life squres.
+    # Print Starting HP to Console
+    # When gaining or loosing health: Print info to console
+        # Starting Health
+        # Ending Health
+        # Amount health gained / losed.
+# Randomize the squares.
+    # Print total number of squares to the Console.
